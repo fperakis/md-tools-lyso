e3f004bb24b7b49d2c106fddceae1c42f7e82a6f
fperakis <f.perakis@fysik.su.se>
2020-06-05 12:36:52 +0200

    made some updates for minmising the disk drive required

--- analysis/md_analysis.sh
+++ analysis/md_analysis.sh
@@ -6,8 +6,8 @@ do
   gmx select -s md.gro -f md.xtc -on md_noMW.ndx -xvg none -select "all and not name MW"
   gmx editconf -f md.tpr -n md_noMW.ndx -o md_noMW.gro -pbc no
   gmx trjconv -s md.tpr -f md.xtc -o md_noMW.xtc -n md_noMW.ndx -pbc mol -ur compact -center
-  gmx energy -f md.edr -o volume.xvg < ../../src/c20.txt
-  gmx energy -f md.edr -o potential.xvg < ../../src/c10.txt
+  gmx energy -f md.edr -o md_volume.xvg < ../../src/c20.txt
+  gmx energy -f md.edr -o md_potential.xvg < ../../src/c10.txt
   gmx saxs -f md.xtc -s md.gro -n md_noMW.ndx -sq saxs_md.xvg -energy 8.04
   cd ..
 done
