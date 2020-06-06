441572d39fb8461632395805275f7ec44b0a3c9b
fperakis <f.perakis@fysik.su.se>
2020-06-06 11:17:43 +0200

    added mdT

--- scripts/loop_mdT.sh
+++ scripts/loop_mdT.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd ../data/
-for R in $(seq 0 1 9)
+for R in $(seq 0 1 0)
 do 
   cd R${R}
   for T in $(seq 180 10 300)
   do
     # update mdp file
-    cp ../../mdp/md.mdp md_${T}.mdp
+    cp ../../mdp/mdT.mdp md_${T}.mdp
     sed -i "s/300/$T/g" md_${T}.mdp
     # run gromp
   	gmx grompp -f md_${T}.mdp -c md.gro -t md.cpt -p topol.top -o md_${T}.tpr
     # update submission script
-    cp ../../batch/submit_mdT.sh md_${T}.sh
+    cp ../../batch/mdT_CPU.sh md_${T}.sh
     sed -i "s/300/$T/g" md_${T}.sh
     # submit job
   	sbatch md_${T}.sh
