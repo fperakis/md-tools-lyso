069f6fcf577b3c2330622c39510ae5cdc05f46f3
fperakis <f.perakis@fysik.su.se>
2020-07-16 10:13:18 +0200

    updated analysis

--- analysis/npt_analysis.sh
+++ analysis/npt_analysis.sh
@@ -3,15 +3,12 @@ cd ../data/
 for R in $(seq 0 1 9)
 do 
 cd R${R}
-gmx select -s npt.gro -f npt.xtc -on npt_noMW.ndx -xvg none -select "all and not name MW"
-gmx editconf -f npt.tpr -n npt_noMW.ndx -o npt_noMW.gro -pbc no
-gmx trjconv -s npt.tpr -f npt.xtc -o npt_noMW.xtc -n npt_noMW.ndx -pbc mol -ur compact -center
 
-gmx energy -f nvt.edr -o nvt_volume.xvg << EOF
+gmx energy -f npt.edr -o npt_volume.xvg << EOF
 20
 EOF
 
-gmx energy -f nvt.edr -o nvt_potential.xvg << EOF
+gmx energy -f npt.edr -o npt_potential.xvg << EOF
 10
 EOF
 
